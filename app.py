c028b5a0a8a58467d5e657106ac0725b11ec347b
Bugrahan Cigdemoglu <mbugrahanc+github@gmail.com>
2019-10-25 17:50:20 -0700

    add debugging

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import argparse
 from flask import Flask, request, jsonify, render_template
 import CS_IA_revised2 as scheduler
 app = Flask(__name__)
@@ -41,64 +42,14 @@ def reset_schedule():
     response["RESULT"] = True
     return jsonify(response)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/receiveexcel/', methods=['POST'])
-def get_csv():
-    print(request)
-    class_data = request.args.get('excel_data')
-    print(class_data)
-
-    return "Data sent for processing"
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {param} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-@app.route('/background_process')
-def background_process():
-	try:
-		lang = request.args.get('proglang', 0, type=str)
-		if lang.lower() == 'python':
-			return jsonify(result='You are wise')
-		else:
-			return jsonify(result='Try again.')
-	except Exception as e:
-		return str(e)
-
 if __name__ == '__main__':
+    debug = False
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help="Turns debugging on", action='store_true')
+    args = parser.parse_args()
+    if args.debug:
+        print("Debugging on")
+        debug = True
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(debug=debug, threaded=True, port=5000)
